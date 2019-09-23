ec90f6957144d01ca15d8101ba0de214eb63f662
oleg <hnapovskiy20@gmail.com>
2019-09-23 14:57:37 +0300

    Updates

---
+++ precache-manifest.5acee24744abb5e63e3a2f0517417d28.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88d06e8483df5323f84d323e3d46a5c0",
+    "url": "/goit-react-hw-02-reader/index.html"
+  },
+  {
+    "revision": "04eb6d705337c6d14983",
+    "url": "/goit-react-hw-02-reader/static/css/main.c4dd3451.chunk.css"
+  },
+  {
+    "revision": "f2937766e74fe852552b",
+    "url": "/goit-react-hw-02-reader/static/js/2.794b3c7f.chunk.js"
+  },
+  {
+    "revision": "04eb6d705337c6d14983",
+    "url": "/goit-react-hw-02-reader/static/js/main.d6743c45.chunk.js"
+  },
+  {
+    "revision": "00f7643cf5fd8a235fee",
+    "url": "/goit-react-hw-02-reader/static/js/runtime-main.85f11a9b.js"
+  }
+]);
